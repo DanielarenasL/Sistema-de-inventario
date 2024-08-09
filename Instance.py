8695a96b0755ad0186e142f473fd4f673decbbfc
DanielarenasL <daniel2004al14@gmail.com>
2024-08-09 16:55:24 -0500

    seguridad api key

--- Instance.py
+++ Instance.py
@@ -1,8 +1,12 @@
-
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
 
-uri = "mongodb+srv://danielarenasl:EY6fmNUA9GwKbrny@madtrigcrew.9iqhp.mongodb.net/?retryWrites=true&w=majority&appName=madtrigcrew"
+load_dotenv() #carga las variables de entorno del .env
+api_key = os.getenv("MONGODB_API_KEY")
+
+uri = f"mongodb+srv://danielarenasl:{api_key}@madtrigcrew.9iqhp.mongodb.net/?retryWrites=true&w=majority&appName=madtrigcrew"
 
 # Create a new connection and connect to the server
 connection = MongoClient(uri, server_api=ServerApi('1'))
